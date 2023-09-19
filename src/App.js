2627401d052dd8938ef42482d257f7c91c826049
SofiaEn9 <sofiaanen9@gmail.com>
2023-09-19 11:52:47 -0600

    Style layout

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Search />
-      <Weather />
+      <div className="contaner">
+        <header>
+          <Search />
+          <Weather />
+        </header>
+      </div>
+      <footer>
+        <small>
+          <a
+            href="https://github.com/SofiaEn9/week4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced code
+          </a>{" "}
+          by Sofía Enríquez
+        </small>
+      </footer>
     </div>
   );
 }
